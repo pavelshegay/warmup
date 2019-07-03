52a73219208d245c8f4fff2b686fab0c2f19f86c
Pavel Shegai <pshegai@gmail.com>
2019-07-03 22:04:50 +0500

    solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 // PLEASE DON'T change function name
+
+const DENOMINATORS = {H: 50, Q: 25, D: 10, N: 5, P: 1};
+const MAX_CURRENCY = 10000;
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    "use strict";
+    if (currency > MAX_CURRENCY) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    let result = {};
+    let remainder = currency;
+    Object.keys(DENOMINATORS).forEach(function (k) {
+        let val = DENOMINATORS[k];
+        if (remainder >= val) {
+            let div = ~~(remainder / val);
+            result[k] = div;
+            remainder = remainder - div * val;
+        }
+    });
+    return result;
+};
